0062b4fda1497072f61096c7721e620e67344d28
Richard Bell <richard.bell1@wipro.com>
2019-06-10 18:18:13 +0100

    Add time for wedding

--- src/date.js
+++ src/date.js
@@ -4,6 +4,7 @@ const WeddingDate = props => (
     <div className="container">
     <div className="block">
     <h2 className="line center">Saturday 15th June 2019</h2>
+    <h2 className="line center">1pm</h2>
     </div>
     </div>
 );
